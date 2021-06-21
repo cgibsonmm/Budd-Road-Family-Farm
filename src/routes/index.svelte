00299a776e48b51a4dcdbe3160a57bb180aeee11
corey gibson <cgibsonmm@gmail.com>
2021-06-21 17:41:20 -0400

    added spinner

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,10 @@
-<div class="text-center">Comming Soon...</div>
+<script>
+	import { Moon } from 'svelte-loading-spinners';
+</script>
+
+<div class="flex flex-col items-center">
+	<div class="m-4">
+		<Moon size="60" color="#000000" unit="px" duration="1s" />
+	</div>
+	<div>Comming Soon...</div>
+</div>
